f08d980a3fd0ac3827466f4b85ecae2aad6ba5fe
Sammeet Koli <sammeet29@gmail.com>
2026-06-03 15:45:07 -0700

    Add reverse-string

---
+++ strings/reverse-string.cpp
@@ -0,0 +1,36 @@
+/***************************************************************************************************
+Write a function that reverses a string. The input string is given as an array of characters s.
+
+You must do this by modifying the input array in-place with O(1) extra memory.
+
+ 
+
+Example 1:
+
+Input: s = ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+Example 2:
+
+Input: s = ["H","a","n","n","a","h"]
+Output: ["h","a","n","n","a","H"]
+***************************************************************************************************/
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        auto const size = s.size();
+        for(int i = 0; i < size/2; i++)
+        {
+            std::swap(s[i], s[size-i-1]);
+        }
+    }
+};
+
+// solution in C
+void reverseString(char* s, int sSize) {
+    for(int i = 0; i < sSize/2; i++)
+    {
+        char tempc = s[i];
+        s[i] = s[sSize - i - 1];
+        s[sSize - i - 1] = tempc;
+    }
+}
